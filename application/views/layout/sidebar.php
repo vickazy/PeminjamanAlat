eb788b73dfb7dc4c97d232b768351e93edf8573c
Ridhan Sholeh <ridhannewbiecheaters@gmail.com>
2017-11-29 21:38:02 +0700

    Views
    Sidebare

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -11,31 +11,55 @@
 				<li class="menu-category">Master</li>
 				<li class="menu-item active">
 					<a class="menu-link" href="<?php base_url() ?>">
-						<span class="icon fa fa-home"></span>
 						<span class="title">Halaman Utama</span>
+						<i class="material-icons">home</i>
+					</a>
+				</li>
+				<li class="menu-item">
+					<a class="menu-link" href="#">
+						<span class="title">Data Alat</span>
+						<i class="material-icons">build</i>
+					</a>
+				</li>
+				<li class="menu-item">
+					<a class="menu-link" href="#">
+						<span class="title">Data Kelas</span>
+						<i class="material-icons">store</i>
+					</a>
+				</li>
+				<li class="menu-item">
+					<a class="menu-link" href="#">
+						<span class="title">Data Keperluan</span>
+						<i class="material-icons">help_outline</i>
+					</a>
+				</li>
+				<li class="menu-item">
+					<a class="menu-link" href="#">
+						<span class="title">Data Petugas</span>
+						<i class="material-icons">people</i>
 					</a>
 				</li>
 
 				<li class="menu-category">Transaksi</li>
 				<li class="menu-item">
 					<a class="menu-link" href="#">
-						<span class="icon fa fa-user"></span>
-						<span class="title">Users</span>
+						<span class="title">Data Petugas</span>
 						<span class="arrow"></span>
+						<i class="material-icons">people</i>
 					</a>
 
 					<ul class="menu-submenu">
 						<li class="menu-item">
 							<a class="menu-link" href="#">
 								<span class="dot"></span>
-								<span class="title">Moderators</span>
+								<span class="title">Lihat Data Petugas</span>
 							</a>
 						</li>
 
 						<li class="menu-item">
 							<a class="menu-link" href="#">
 								<span class="dot"></span>
-								<span class="title">Customers</span>
+								<span class="title">Tambah Data Petugas</span>
 							</a>
 						</li>
 					</ul>
