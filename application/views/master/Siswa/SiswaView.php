46625b4eb48d32fe164f6ba98fb81f464ee1c28e
Ridhan Sholeh <ridhan.sholeh.rs@gmail.com>
2018-02-08 13:09:06 +0700

    Launching very soon

--- application/views/master/Siswa/SiswaView.php
+++ application/views/master/Siswa/SiswaView.php
@@ -18,7 +18,23 @@
 
 				<h4 class="card-title"><strong>Alat Yang Kamu Minjam</strong></h4>
 
-				
+				<div class="card-body">
+					<h1><img src="<?php echo base_url(); ?>assets/img/logo-lg.png" alt="logo"></h1>
+
+					<br><br><br><br><br>
+
+					<h2 class="text-uppercase text-black fs-50 d-none d-md-block">
+						<span class="fs-70 fw-900">Launching</span><br>
+						<span>very soon</span>
+						<span class="text-primary fs-70">.</span>
+					</h2>
+
+					<h2 class="text-uppercase text-black fs-30 d-md-none">
+						<span class="fs-50 fw-900">Launching</span><br>
+						<span>very soon</span>
+						<span class="text-primary fs-50">.</span>
+					</h2>
+				</div>
 
 			</div>
 		</div>
